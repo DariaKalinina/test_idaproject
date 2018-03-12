f1873fc1b795e2f85b0e6c173901aa3bfa377573
Daria Kalinina <lynxx91@mail.ru>
2018-03-13 01:02:02 +0300

    js is done, start doing adaptive

--- src/js/order.js
+++ src/js/order.js
@@ -1,76 +1,118 @@
 'use strict';
 
+var inputPerson = $('.card__person');
+var inputCVV = $('.card__cvc-input');
+var inputNumber = $('.card__number-input');
 
-$('.card__number-input').on('keypress', function(e) {
-  var keyCode =e.originalEvent.keyCode;
-  console.log(e.originalEvent);
-  $('.card__number-input').removeClass('card__number-input--color-red');
-  if(keyCode<=48 || keyCode>=57){
-    $('.card__number-input').addClass('card__number-input--color-red');
+function validateField(key, start, end, backspace) {
+  if(key>=start && key<=end){
+    return true;
+  }
+  if(key===backspace){
+    return true;
+  }
+  return false;
+}
+
+inputNumber.on('keypress', function(e) {
+  var keyCode = e.originalEvent.keyCode;
+  var isValid = validateField(keyCode, 48, 57);
+  if(isValid){
+    inputNumber.removeClass('error');
+  } else {
+    inputNumber.addClass('error');
     return false;
   }
 });
 
-$('.card__cvc-input').on('keypress', function(e) {
-  var keyCode =e.originalEvent.keyCode;
-  console.log(e.originalEvent);
-  $('.card__cvc-input').removeClass('card__cvc-input--color-red');
-  if(keyCode<=48 || keyCode>=57){
-    $('.card__cvc-input').addClass('card__cvc-input--color-red');
+inputCVV.on('keypress', function(e) {
+  var keyCode = e.originalEvent.keyCode;
+  var isValid = validateField(keyCode, 48, 57);
+  if(isValid){
+    inputCVV.removeClass('error');
+  } else {
+    inputCVV.addClass('error');
     return false;
   }
 });
 
-$('.card__person').on('keypress', function(e) {
+inputPerson.on('keypress', function(e) {
   var keyCode =e.originalEvent.keyCode;
-  console.log(keyCode);
-  $('.card__person').removeClass('card__person--color-red');
-  if(keyCode!=32 && keyCode<97 || keyCode>122){
-    $('.card__person').addClass('card__person--color-red');
+  var isValid = validateField(keyCode, 97, 122, 32);
+  if(isValid){
+    inputPerson.removeClass('error');
+  } else {
+    inputPerson.addClass('error');
     return false;
-  };
+  }
 });
 
+function validateLength(elem, length) {
+  if(elem.val().length >= length) { // если < 3 символов
+    return true;
+  }
+  return false;
+}
 
 $(document).ready(function(){
-  var inputPerson = $('.card__person');
-  var inputCVV = $('.card__cvc-input');
 
   $(document).on('change','.card__person',function(){
-    if(inputPerson.val().length < 4) { // если < 4 символов
-      $('.card__person').addClass('card__person--color-red');
-      return false;
-    }
-    else {
-      $('.card__person').removeClass('card__person--color-red');
+    var isLenght = validateLength(inputPerson, 4);
+    if(isLenght) {
+      inputPerson.removeClass('error');
       return true;
-    }
+      }
+    inputPerson.addClass('error');
+    return false;
   });
 
-  $(document).on('change','.card__number-input',function(){
-    // for (var i = 0; i < 4; i++) {
-      // var inputNumber = $('.card__number-input--[i]');
-      // if(inputNumber.val().length < 4) { // если < 4 символов
-      //   $('.card__number-input--[i]').addClass('card__number-input--color-red');
-      //   return false;
-      // }
-      // else {
-      //   $('.card__number-input--[i]').removeClass('card__number-input--color-red');
-      //   return true;
-      // }
-    }
-  });
 
+  $(document).on('change','.card__number-input', function(){
+    inputNumber.each(function() {
+      var isLenght = validateLength($(this), 4);
+      if(isLenght) {
+        $(this).removeClass('error');
+        return true;
+        }
+      $(this).addClass('error');
+    });
+  });
 
   $(document).on('change','.card__cvc-input',function(){
-    if(inputCVV.val().length < 3) { // если < 3 символов
-      $('.card__cvc-input').addClass('card__cvc-input--color-red');
-      return false;
-    }
-    else {
-      $('.card__cvc-input').removeClass('card__cvc-input--color-red');
+    var isLenght = validateLength(inputCVV, 3);
+    if(isLenght) {
+      inputCVV.removeClass('error');
       return true;
-    }
+      }
+    inputCVV.addClass('error');
+    return false;
   });
+});
 
+
+
+function validateForm() {
+  // ask your validation funcs
+  var person = inputPerson.val();
+  var cvc = inputCVV.val();
+  for (var i = 0; i < inputNumber.length; i++) {
+    var num = inputNumber[i].value;
+    if(num.length<4) return false;
+  }
+
+  if(person.length<4) return false;
+  if(cvc.length<3) return false;
+  return true;
+}
+
+
+$('.order__submit').on('click', function(e) {
+  e.preventDefault();
+  var isValidForm = validateForm();
+  if (isValidForm) {
+    // $('.order__submit').submit();
+    console.log("ok");
+  } else{
+    console.log("no ok");
+  }
 });
